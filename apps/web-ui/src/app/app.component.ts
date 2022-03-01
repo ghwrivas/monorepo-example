b3aef7b5abec72cbd2a8ff086f967059246a97f9
Williams Rivas <wrivasa@falabella.cl>
2022-03-01 19:35:31 -0300

    agrega metodo para consultar todos y agregar todo

--- apps/web-ui/src/app/app.component.ts
+++ apps/web-ui/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'mx-onboarding-workspace-root',
@@ -7,4 +12,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-ui';
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
--- apps/web-ui/src/app/app.module.ts
+++ apps/web-ui/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedComponentsModule } from '@mx-onboarding-workspace/shared/components';
 import { ShellWebModule } from '@mx-onboarding-workspace/shell-web';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedComponentsModule, ShellWebModule],
+  imports: [BrowserModule, SharedComponentsModule, ShellWebModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
